e17b27a0510c4d2303643d44d1721c4ffa514b25
projectdbmssose2017 <thomas.sieron@fu-berlin.de>
2017-06-01 22:27:37 +0200

    lange Wörter geadded bei tweet

--- election.sql
+++ election.sql
@@ -1,5 +1,5 @@
 CREATE TABLE account ( Handle varchar (16) NOT NULL PRIMARY KEY, BenutzerName varchar (15) NOT NULL);
-CREATE TABLE tweet ( TweetID int NOT NULL PRIMARY KEY, Text varchar(140) NOT NULL, Zeitpunkt timestamp without time zone NOT NULL, AnzahlFavoriten int, AnzahlRetweets int, SourceURL varchar(2000), isRetweet boolean, Originalautor varchar(140) );
+CREATE TABLE tweet ( TweetID int NOT NULL PRIMARY KEY, Text varchar(140) NOT NULL, Zeitpunkt timestamp without time zone NOT NULL, AnzahlFavoriten int, AnzahlRetweets int, SourceURL varchar(2000), isRetweet boolean, Originalautor varchar(140), AnzahlLangeWoerter int );
 CREATE TABLE twittert ( TweetID int NOT NULL PRIMARY KEY, Handle varchar(16) NOT NULL);
 CREATE TABLE Hashtag (Tag varchar(139) NOT NULL PRIMARY KEY);
 CREATE TABLE Erwähnt_account ( TweetID int NOT NULL, Handle varchar(16) NOT NULL, PRIMARY KEY (TweetID, Handle));
